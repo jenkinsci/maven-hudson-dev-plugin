84eea148ad4e6afe4524641041d12f60c5b4223e
Jesse Glick <jglick@cloudbees.com>
2019-11-08 18:17:05 -0500

    9.4.22.v20191022 upgrade.

--- src/test/java/org/eclipse/jetty/maven/plugin/it/TestGetContent.java
+++ src/test/java/org/eclipse/jetty/maven/plugin/it/TestGetContent.java
@@ -1,6 +1,6 @@
 //
 //  ========================================================================
-//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  Copyright (c) 1995-2019 Mort Bay Consulting Pty. Ltd.
 //  ------------------------------------------------------------------------
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -18,16 +18,19 @@
 
 package org.eclipse.jetty.maven.plugin.it;
 
-import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jetty.client.HttpClient;
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.io.LineNumberReader;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jetty.client.HttpClient;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
@@ -39,34 +42,48 @@ public class TestGetContent
         throws Exception
     {
         int port = getPort();
-        Assert.assertTrue( port > 0 );
+        assertTrue(port > 0);
         HttpClient httpClient = new HttpClient();
         try
         {
             httpClient.start();
 
-            if (Boolean.getBoolean( "helloServlet" ))
+            if (Boolean.getBoolean("helloServlet"))
             {
-                String response = httpClient.GET( "http://localhost:" + port + "/hello?name=beer" ).getContentAsString();
-                Assert.assertEquals( "Hello beer", response.trim() );
-                response = httpClient.GET( "http://localhost:" + port + "/hello?name=foo" ).getContentAsString();
-                Assert.assertEquals( "Hello foo", response.trim() );
-                System.out.println( "helloServlet" );
+                String response = httpClient.GET("http://localhost:" + port + "/hello?name=beer").getContentAsString();
+                assertEquals("Hello beer", response.trim(), "it test " + System.getProperty("maven.it.name"));
+                response = httpClient.GET("http://localhost:" + port + "/hello?name=foo").getContentAsString();
+                assertEquals("Hello foo", response.trim(), "it test " + System.getProperty("maven.it.name"));
+                System.out.println("helloServlet");
             }
-            if (Boolean.getBoolean( "pingServlet" ))
+            if (Boolean.getBoolean("pingServlet"))
             {
-                System.out.println( "pingServlet ok" );
-                String response = httpClient.GET( "http://localhost:" + port + "/ping?name=beer" ).getContentAsString();
-                Assert.assertEquals( "pong beer", response.trim() );
-                System.out.println( "pingServlet" );
+                System.out.println("pingServlet");
+                String response = httpClient.GET("http://localhost:" + port + "/ping?name=beer").getContentAsString();
+                assertEquals("pong beer", response.trim(), "it test " + System.getProperty("maven.it.name"));
+                System.out.println("pingServlet ok");
             }
-            String contentCheck = System.getProperty( "contentCheck" );
-            if(StringUtils.isNotBlank( contentCheck ) )
+            String contentCheck = System.getProperty("contentCheck");
+            String pathToCheck = System.getProperty("pathToCheck");
+            if (StringUtils.isNotBlank(contentCheck))
             {
-                String response = httpClient.GET( "http://localhost:" + port ).getContentAsString();
-                Assert.assertTrue( "response not contentCheck: " + contentCheck + ", response:" + response, //
-                                   response.contains( contentCheck ) );
-                System.out.println( "contentCheck" );
+                String url = "http://localhost:" + port;
+                if (pathToCheck != null)
+                {
+                    url += pathToCheck;
+                }
+                String response = httpClient.GET(url).getContentAsString();
+                assertTrue(response.contains(contentCheck), "it test " + System.getProperty("maven.it.name") +
+                        ", response not contentCheck: " + contentCheck + ", response:" + response);
+                System.out.println("contentCheck");
+            }
+            if (Boolean.getBoolean("helloTestServlet"))
+            {
+                String response = httpClient.GET("http://localhost:" + port + "/testhello?name=beer").getContentAsString();
+                assertEquals("Hello from test beer", response.trim(), "it test " + System.getProperty("maven.it.name"));
+                response = httpClient.GET("http://localhost:" + port + "/testhello?name=foo").getContentAsString();
+                assertEquals("Hello from test foo", response.trim(), "it test " + System.getProperty("maven.it.name"));
+                System.out.println("helloServlet");
             }
         }
         finally
@@ -75,36 +92,36 @@ public class TestGetContent
         }
     }
 
-
     public static int getPort()
         throws Exception
     {
         int attempts = 70;
         int port = -1;
-        String s = System.getProperty( "jetty.port.file" );
-        Assert.assertNotNull( s );
-        Path p = Paths.get( s );
-        while ( true )
+        String s = System.getProperty("jetty.port.file");
+        assertNotNull(s);
+        Path p = Paths.get(s);
+        while (true)
         {
-            if ( Files.exists(p) )
+            if (Files.exists(p))
             {
-                try (Reader r = Files.newBufferedReader( p ); LineNumberReader lnr = new LineNumberReader( r );)
+                try (Reader r = Files.newBufferedReader(p);
+                     LineNumberReader lnr = new LineNumberReader(r);)
                 {
                     s = lnr.readLine();
-                    Assert.assertNotNull( s );
-                    port = Integer.parseInt( s.trim() );
+                    assertNotNull(s);
+                    port = Integer.parseInt(s.trim());
                 }
                 break;
             }
             else
             {
-                if ( --attempts < 0 )
+                if (--attempts < 0)
                 {
                     break;
                 }
                 else
                 {
-                    Thread.currentThread().sleep( 1000 );
+                    Thread.currentThread().sleep(1000);
                 }
             }
         }
